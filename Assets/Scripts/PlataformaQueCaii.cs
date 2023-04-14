a94489f6226ca460111dec676a6b8e0413a024e9
Boomber42 <cleiton.-.mar@outlook.com>
2023-04-14 20:17:48 -0300

    aaaaa

--- Assets/Scripts/PlataformaQueCaii.cs
+++ Assets/Scripts/PlataformaQueCaii.cs
@@ -8,9 +8,22 @@ public class PlataformaQueCaii : MonoBehaviour
 
     private TargetJoint2D gatilho;
     private BoxCollider2D colisaoDaPlataforma;
+    private int indexLayerFimDeJogo = 8;
 
     void Start()
     {
-        
+       this.gatilho = GetComponent<TargetJoint2D>();
+       this.colisaoDaPlataforma = GetComponent<BoxCollider2D>();       
     }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            Invoke("Falling", tempoDeQueda);
+        }
+
+        if(collision.gameObject.layer ==)
+    }
+
 }
