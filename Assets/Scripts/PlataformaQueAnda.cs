86f971c4cc339127655d63d6fbe06411d6af6a24
Boomber42 <cleiton.-.mar@outlook.com>
2023-04-14 20:12:56 -0300

    subindo animacao

---
+++ Assets/Scripts/PlataformaQueAnda.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlataformaQueAnda : MonoBehaviour
+{
+    private SliderJoint2D movimentarPlataforma;
+
+    void Start()
+    {
+        this.movimentarPlataforma = GetComponent<SliderJoint2D>();
+    }
+
+    private void Movimentar()
+    {
+        Debug.Log(this.movimentarPlataforma.connectedAnchor.x);
+
+        if(this.transform.position.x*2 > this.movimentarPlataforma.connectedAnchor.x)
+        {
+
+        }
+    }
+
+    void Update()
+    {
+        Movimentar();
+    }
+}
---
+++ Assets/Scripts/PlataformaQueCaii.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlataformaQueCaii : MonoBehaviour
+{
+    public float tempoDeQueda;
+
+    private TargetJoint2D gatilho;
+    private BoxCollider2D colisaoDaPlataforma;
+
+    void Start()
+    {
+        
+    }
+}
