ada81df2dc44782ffa2fef869620f3d9e7a561bd
Edson Vitor <edson@taqe.com.br>
2023-04-14 21:08:16 -0300

    teste

--- Assets/Scripts/PlataformaQueAnda.cs
+++ Assets/Scripts/PlataformaQueAnda.cs
@@ -5,24 +5,38 @@ using UnityEngine;
 public class PlataformaQueAnda : MonoBehaviour
 {
     private SliderJoint2D movimentarPlataforma;
+    private JointMotor2D motor2D;
+    public Rigidbody2D heroi;
 
     void Start()
     {
         this.movimentarPlataforma = GetComponent<SliderJoint2D>();
+        this.motor2D = this.movimentarPlataforma.motor;
     }
 
     private void Movimentar()
     {
-        Debug.Log(this.movimentarPlataforma.connectedAnchor.x);
-
-        if (this.transform.position.x * 2 > this.movimentarPlataforma.connectedAnchor.x)
+        if (this.movimentarPlataforma.limitState == JointLimitState2D.UpperLimit)
         {
-
+            this.motor2D.motorSpeed = -1;
         }
+
+        if (this.movimentarPlataforma.limitState == JointLimitState2D.LowerLimit)
+        {
+            this.motor2D.motorSpeed = 1;
+        }
+
+        this.movimentarPlataforma.motor = this.motor2D;
     }
 
     void Update()
     {
         Movimentar();
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        collision.gameObject.transform.parent = this.transform;
+        //collision.gameObject.transform.position = new Vector2(this.transform.position.x, collision.gameObject.transform.position.y);
     }
 }
