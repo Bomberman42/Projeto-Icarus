9d17043783761552fc5ac47ec829602060a87ae2
Edson Vitor <edson@taqe.com.br>
2023-04-16 14:54:56 -0300

    Variáveis compartilhadas

---
+++ Assets/Scripts/VariaveisCompartilhadas/VariaveisCompartilhadas.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName="VariaveisCompartilhadasFloat", menuName="Variáveis Compartilhadas / Valor Float")]
+public class VariaveisCompartilhadas : ScriptableObject
+{
+    public float valor;
+}
