9dd7eb75da35ed7e9b19f2d5e7649fe9c4600737
Boomber42 <cleiton.-.mar@outlook.com>
2023-04-17 16:14:14 -0300

    oh que miseria, todos querem ser meu inimigo

--- Assets/Scripts/Inimigo.cs
+++ Assets/Scripts/Inimigo.cs
@@ -4,19 +4,59 @@ using UnityEngine;
 
 public class Inimigo : MonoBehaviour
 {
+
+    private Rigidbody2D fisicaDoInimigo;
+    private Animator animacaoDoInimigo;
+    private bool colidir;
+
+    public Transform colisorDaDireita;
+    public Transform colisorDaEsquerda;
+    public LayerMask camadas;
+    public Transform pontoDaCabeca;
+    public BoxCollider2D boxCollider2D;
+
     public int totalDanoPorAtaque;
     public int totalDeVida;
     public int velocidade;
 
-    // Start is called before the first frame update
+
+
     void Start()
     {
-        
+        this.fisicaDoInimigo = GetComponent<Rigidbody2D>();
+        this.animacaoDoInimigo = GetComponent <Animator>();
     }
 
-    // Update is called once per frame
+
     void Update()
     {
-        
+        this.fisicaDoInimigo.velocity = new Vector2(this.velocidade, this.fisicaDoInimigo.velocity.y);
+        this.colidir = Physics2D.Linecast(this.colisorDaDireita.position, this.colisorDaEsquerda.position, this.camadas);
+        Debug.DrawRay(new Vector2(this.colisorDaDireita.position.x, this.colisorDaDireita.position.y), Vector2.left ,Color.red);
+        this.animacaoDoInimigo.SetBool("correr", true);
+
+        if (colidir)
+        {
+            transform.localScale = new Vector2(transform.localScale.x * -1f, transform.localScale.y);
+            this.velocidade = -this.velocidade;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D colisor)
+    {
+        if (colisor.gameObject.tag == "Player")
+        {
+            float altura = colisor.contacts[0].point.y - this.pontoDaCabeca.position.y;
+
+            if (altura > 0)
+            {
+                Debug.Log("sim");
+                colisor.gameObject.GetComponent<Rigidbody2D>().AddForce(Vector2.up * 10, ForceMode2D.Impulse);
+                velocidade = 0;
+                this.animacaoDoInimigo.SetTrigger("hit");
+                boxCollider2D.enabled = false;
+
+            }
+        }
     }
 }
