c8cddb93f861bd4fe9b34913a7e7b7e9354df0e5
Boomber42 <cleiton.-.mar@outlook.com>
2023-04-19 19:28:58 -0300

    tela de transição

--- Assets/Scripts/MenuInicial.cs
+++ Assets/Scripts/MenuInicial.cs
@@ -9,11 +9,45 @@ public class MenuInicial : MonoBehaviour
     private AudioSource musicaDoNaruto;
     [SerializeField]
     private AudioSource musicaDoJogo;
+    public CanvasGroup grupoDoCanvas;
+    public float tempoDeTransicao;
 
     void Start()
     {
         this.musicaDoNaruto.Play();
         this.musicaDoJogo.enabled = false;
+        Invoke("ExecutaTransicao", 3f);
+        
+    }
+
+    private void ExecutaTransicao()
+    {
+        StartCoroutine(TransicaoDeCena(1f, 0f, "FadeOut"));
+    }
+
+    private IEnumerator TransicaoDeCena(float tempoInicio, float tempoFinal, string transicao)
+    {
+        float velocidade = (tempoFinal - tempoInicio) / tempoDeTransicao;
+        grupoDoCanvas.alpha = tempoInicio;
+
+        if(transicao == "FadeIn")
+        {
+            while(grupoDoCanvas.alpha < tempoFinal)
+            {
+                grupoDoCanvas.alpha += velocidade * Time.deltaTime;
+                yield return null;
+            }
+        }
+        else
+        {
+            while (grupoDoCanvas.alpha > tempoFinal)
+            {
+                grupoDoCanvas.alpha += velocidade * Time.deltaTime;
+                yield return null;
+            }
+        }
+
+        grupoDoCanvas.alpha = tempoFinal;
     }
 
     void Update()
